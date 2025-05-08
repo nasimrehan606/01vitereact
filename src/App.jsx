6883ca265eed2d351c385739649bc70e7b8cabb1
Moiz <moeeznasim32@gmail.com>
2025-05-08 05:49:56 +0500

    creating Navbar and TextForm, About section

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Navbar from "./component/Navbar";
 import TextForm from "./component/TextForm";
+import About from "./component/About";
 
 let name = "Rehan";
 const App = () => {
@@ -8,7 +9,8 @@ const App = () => {
     <>
       <Navbar title="TextUtils" aboutText="About Us" />
       <div className="container my-3">
-        <TextForm heading="Enter The text to analyze below" />
+        {/* <TextForm heading="Enter The text to analyze below" /> */}
+        <About />
       </div>
     </>
   );
--- src/component/TextForm.jsx
+++ src/component/TextForm.jsx
@@ -1,19 +1,91 @@
 import React, { useState } from "react";
 
 const TextForm = (props) => {
-  const [text, setText] = useState("Enter text here");
+  const handleUpClick = () => {
+    // console.log("UpperCase was Clicked" + text);
+    let newText = text.toUpperCase();
+    setText(newText);
+  };
 
+  const handleLoClick = () => {
+    // console.log("UpperCase was Clicked" + text);
+    let newText = text.toLowerCase();
+    setText(newText);
+  };
+
+  const handleClearClick = () => {
+    // console.log("UpperCase was Clicked" + text);
+    let newText = "";
+    let newTitle = "";
+    setTitle(newTitle);
+    setText(newText);
+  };
+
+  const handleOnChange = (event) => {
+    // console.log("On Change");
+    setText(event.target.value);
+  };
+
+  const handleOnChanget = (event) => {
+    // console.log("On Change");
+    setTitle(event.target.value);
+  };
+
+  const [title, setTitle] = useState("");
+  const [text, setText] = useState("");
   return (
-    <div>
-      <h1>{props.heading}</h1>
-      <div className="mb-3">
-        <label htmlFor="myBox" className="form-label">
-          Enter your text here
-        </label>
-        <textarea className="form-control" id="myBox" rows="8"></textarea>
+    <>
+      <div className="container">
+        <h1>{props.heading}</h1>
+
+        <div className="input-group">
+          <textarea
+            className="form-control my-2"
+            value={title}
+            onChange={handleOnChanget}
+            id="myBox"
+            rows="1"
+          ></textarea>
+        </div>
+
+        <div className="mb-3">
+          <textarea
+            className="form-control"
+            value={text}
+            onChange={handleOnChange}
+            id="myBox"
+            rows="8"
+          ></textarea>
+        </div>
+
+        <button className="btn btn-primary my-1" onClick={handleUpClick}>
+          Convert to UpperCase
+        </button>
+
+        <button className="btn btn-primary mx-3 my-1" onClick={handleLoClick}>
+          Convert to LowerCase
+        </button>
+
+        <button
+          className="btn btn-primary mx-3 my-1"
+          onClick={handleClearClick}
+        >
+          Clear Text
+        </button>
       </div>
-      <button className="btn btn-primary">Convert to UpperCase</button>
-    </div>
+
+      <div className="container my-2">
+        <h2>Your Text Summary</h2>
+        <p>
+          {text.split(" ").length} Words and {text.length} Character
+        </p>
+        <p>{0.008 * text.split(" ").length} Minutes Read</p>
+
+        <h2>Preview</h2>
+        <h4> Topic : {title}</h4>
+        <pre>{text}</pre>
+      </div>
+    </>
   );
 };
 
